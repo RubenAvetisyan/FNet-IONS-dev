2e40d1ab16c0d0c2d1329e483d0cf642764ca44a
RUBEN AVETISYAN <arbods.ruben@gmail.com>
2023-03-25 12:23:21 +0400

    TODO: remake

--- admin/server/api/get-totals-abilling.ts
+++ admin/server/api/get-totals-abilling.ts
@@ -6,56 +6,34 @@ import { p as myPromise, p } from '@antfu/utils';
 enum SqlFilePaths {
   ACTIVE = '../../admin/assets/SQL/ABilling/ACTIVE_CLIENTS.sql',
   PASSIVE = '../../admin/assets/SQL/ABilling/PASSIVE_CLIENTS.sql',
-  ALL = '../../admin/assets/SQL/ABilling/ALL_CLIENTS.sql'
-}
-
-enum QueryType {
-  ALL = 'all',
-  ACTIVE = 'active',
-  PASSIVE = 'passive'
-}
-
-function getSqlQueryStringFilePath(queryType: QueryType): string {
-  let sqlFilePath = '';
-  switch (queryType) {
-    case QueryType.ACTIVE:
-      sqlFilePath = SqlFilePaths.ACTIVE;
-      break;
-    case QueryType.PASSIVE:
-      sqlFilePath = SqlFilePaths.PASSIVE;
-      break;
-    case QueryType.ALL:
-      sqlFilePath = SqlFilePaths.ALL;
-      break;
-    default:
-      throw new Error(`Unsupported query type: ${queryType}`);
-  }
-
-  return sqlFilePath;
+  ALL = '../../admin/assets/SQL/ABilling/ALL_CLIENTS.sql',
+  PAYMENTS = '../../admin/assets/SQL/ABilling/payment_cid_summa_by_date.sql'
 }
 
 export default defineEventHandler(async (event) => {
   try {
-    // const { status } = getQuery(event) as { status: QueryType }
-    // console.log('status: ', status);
-
-    // const queryFileSrc = getSqlQueryStringFilePath(status)
 
     const items = [SqlFilePaths.ALL, SqlFilePaths.ACTIVE, SqlFilePaths.PASSIVE]
 
-    const sqlFiles: string[] = await myPromise(items.map(readSqlFile))
+    const sqlFiles: string[] = await myPromise(items.map(readSqlFile), { concurrency: 3 })
+    const [paymentsSQLFiles]: any[] = await myPromise([readSqlFile(SqlFilePaths.PAYMENTS)])
+    console.log('paymentsSQLFiles: ', paymentsSQLFiles);
 
     type Data = { contractBumber: string, clientName: string }[]
-    const sqlQueries = sqlFiles.map(sqlFile => async () => {
-      try {
-        return await executeQuery<Data>(sqlFile, 'abilling')
-      } catch (error: any) {
-        throw new Error(error || '')
-      }
-    })
-    const result = await myPromise(sqlFiles.map(sqlFile => executeQuery<Data>(sqlFile, 'abilling'))).filter(item => !(item instanceof H3Error))
+    const result: any = await myPromise(sqlFiles.map(sqlFile => executeQuery<Data>(sqlFile, 'abilling'))).filter(item => !(item instanceof H3Error))
 
-    return result
+    let contractNumbersArray: string[][] = await myPromise(result.map((res: any) => {
+      return res.body.map(({ contractNumber }: any) => contractNumber)
+    }), { concurrency: 3 })
+
+    const paymentsResult: any[] = await myPromise(contractNumbersArray.map(async contractNumbers => {
+      const queryString = paymentsSQLFiles.replace('@contractNumber', contractNumbers.join(','))
+      return await executeQuery(queryString, 'abilling')
+    }), { concurrency: 3 })
+
+    console.log('paymentsResult: ', paymentsResult[0].header);
+    return [...result, ...paymentsResult]
+
   } catch (error) {
     console.log('error: ', error);
     if (error instanceof Error)
