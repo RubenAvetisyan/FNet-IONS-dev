cd6bb024e4374f3cf20d7064ef417b3c6df925ec
RUBEN AVETISYAN <arbods.ruben@gmail.com>
2023-03-27 00:51:49 +0400

    This commit updates the import statement for the DbName constant in the connection module to import it from the connection-class file instead of using a relative path. This improves code readability and makes it easier to change the location of the DbName constant in the future.

--- admin/utils/ABilling/abilling-connection.ts
+++ admin/utils/ABilling/abilling-connection.ts
@@ -1,3 +1,3 @@
-import MySQLConnection from '../../../utils/MySQL/connection-class'
+import MySQLConnection, { DbName } from '@/utils/MySQL/connection-class';
 
 export const connection = new MySQLConnection(DbName.A_BILLING)
