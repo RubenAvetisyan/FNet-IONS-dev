7d8c88492befead4ab1f606fe1f264a74a658f46
RUBEN AVETISYAN <arbods.ruben@gmail.com>
2023-04-09 23:06:09 +0400

    filtering the auth request

--- server/middleware/test.ts
+++ server/middleware/test.ts
@@ -1,3 +1,6 @@
+import { it } from "node:test"
+
 export default defineEventHandler(async (event) => {
-  console.log(`New request: ${event.node.req.url}`)
+  if (!event.node.req.url?.includes('/api/auth'))
+    console.log(`New request: ${event.node.req.url}`)
 })
