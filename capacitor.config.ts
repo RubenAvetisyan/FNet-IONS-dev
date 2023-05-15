f2abe78f53bef1be76b2f0ed1ad1e13b5ab66d34
RUBEN AVETISYAN <arbods.ruben@gmail.com>
2023-05-16 00:12:52 +0400

    updates

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'd4f244c2',
+  appName: 'IONS',
+  webDir: '.nuxt\dist',
+  bundledWebRuntime: false,
+}
+
+export default config
